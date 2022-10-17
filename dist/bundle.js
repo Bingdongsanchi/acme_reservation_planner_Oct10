8a725bd774aa63e6b0fde853fcb3476a26f0c4ae
Bingdongsanchi <chrisau418@gmail.com>
2022-10-16 17:23:32 -0700

    my version

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: The top-level-await experiment is not enabled (set experiments.topLevelAwait: true to enabled it)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\nError: The top-level-await experiment is not enabled (set experiments.topLevelAwait: true to enabled it)\\n    at /Users/topherou/acme_reservation_planner_Oct10/node_modules/webpack/lib/dependencies/HarmonyDetectionParserPlugin.js:54:11\\n    at Hook.eval [as call] (eval at create (/Users/topherou/acme_reservation_planner_Oct10/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:7:16)\\n    at Hook.CALL_DELEGATE [as _call] (/Users/topherou/acme_reservation_planner_Oct10/node_modules/tapable/lib/Hook.js:14:14)\\n    at JavascriptParser.walkAwaitExpression (/Users/topherou/acme_reservation_planner_Oct10/node_modules/webpack/lib/javascript/JavascriptParser.js:2342:29)\\n    at JavascriptParser.walkExpression (/Users/topherou/acme_reservation_planner_Oct10/node_modules/webpack/lib/javascript/JavascriptParser.js:2272:10)\\n    at JavascriptParser.walkVariableDeclaration (/Users/topherou/acme_reservation_planner_Oct10/node_modules/webpack/lib/javascript/JavascriptParser.js:2126:33)\\n    at JavascriptParser.walkStatement (/Users/topherou/acme_reservation_planner_Oct10/node_modules/webpack/lib/javascript/JavascriptParser.js:1620:10)\\n    at JavascriptParser.walkStatements (/Users/topherou/acme_reservation_planner_Oct10/node_modules/webpack/lib/javascript/JavascriptParser.js:1481:9)\\n    at JavascriptParser.parse (/Users/topherou/acme_reservation_planner_Oct10/node_modules/webpack/lib/javascript/JavascriptParser.js:3375:9)\\n    at /Users/topherou/acme_reservation_planner_Oct10/node_modules/webpack/lib/NormalModule.js:1087:26\");\n\n//# sourceURL=webpack://acme_reservation_planner_oct10/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
